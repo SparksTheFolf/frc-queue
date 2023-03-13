8862e5a242f40aa74a3281693604a0def5d8e8a8
Sparks <officialstackemup@gmail.com>
2023-03-13 12:47:16 -0700

    Add files via upload

---
+++ schedule/action.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+    <button onclick="window.location.href='/schedule';">
+      Go Back
+    </button>
+
+<div name="main" id="main">
+
+<?php
+
+
+$number= $_GET['number'];
+$code= $_GET['code'];
+$level= $_GET['level'];
+$frcCode = getenv('HTTP_frcBASIC');
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://frc-api.firstinspires.org/v3.0/2023/schedule/'.$code.'?tournamentLevel='.$level.'&teamNumber='.$number,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'If-Modified-Since: ',
+    'Authorization: Basic '.$frcCode
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+$final = json_decode($response,true);
+
+$output = "<ul>";
+
+foreach($final['Schedule'] as $Schedule){
+
+	$output.="<h3>".$Schedule['description']."</h3>";
+	$output.="<h5>".'Start Time: '.$Schedule['startTime']."</h5>";
+
+	foreach($Schedule['teams'] as $teams){
+
+
+	$output.="<li>".'Teams: '.$teams['teamNumber']."</li>";
+	$output.="<li>".'Station: '.$teams['station']."</li>";
+	
+	
+
+	}
+
+}
+
+$ouput = "</ul>";
+
+echo $output;
+?>
+
+</div>
+
+</body>
+</html>
---
+++ schedule/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HTML Form</title>
+</head>
+<body>
+    <h1>HTML Form</h1>
+    <form method="" action="action.php">
+        Code: <input type="text" name="code"><br>
+	<p>Please select your type:</p>
+  <input type="radio" id="level" name="level" value="qual">
+  <label for="qual">Qualifications</label><br>
+  <input type="radio" id="level" name="level" value="playoff">
+  <label for="playoff">Playoffs</label><br></br>  
+	number: <input type="text" name="number"><br/>      
+	<br/>
+        <input type="submit" value="submit" >
+    </form>
+</body>
+</html>
+
+</body>
+</html>
