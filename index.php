d8e7b588fdd36914fef7332f1319da2845b18e95
Nolan <officialstackemup@gmail.com>
2023-03-13 13:43:24 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">FRC Queue</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -45,9 +45,10 @@
   </div>
 
   <script>
-  $('.dropdown-toggle').dropdown();
-
-  </script>
+$(document).ready(function(){
+  $(".dropdown-toggle").dropdown("toggle");
+});
+</script>
 <?php
 
 
