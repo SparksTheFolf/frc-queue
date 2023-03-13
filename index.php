16b63102a2787b504f981f121b8f6f84d4265448
Nolan <officialstackemup@gmail.com>
2023-03-13 13:21:43 -0700

    hvjh

--- index.php
+++ index.php
@@ -36,6 +36,17 @@
           <a class="dropdown-item" href="/about">About the Project</a>
         </div>
       </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>s
       </ul>
     </div>
   </div>
