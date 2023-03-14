645e8b28ebc54ed3d596ff882915669f75502751
Nolan <officialstackemup@gmail.com>
2023-03-13 17:34:32 -0700

    Update Header.php

--- Header.php
+++ Header.php
@@ -11,12 +11,9 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
     <div class="container-fluid">
     <a class="navbar-brand" href="/">FRC Queue</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
