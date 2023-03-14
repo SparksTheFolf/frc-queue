698bdc858d48bb24b11090833f411654406b4e01
Nolan <officialstackemup@gmail.com>
2023-03-13 17:26:00 -0700

    wedwe

---
+++ about/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+<link rel="icon" href="icon.ico" type="image/x-icon">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }body{position: relative;}
+    </style>
+</head>
+<body>
+
+
+  <?php include 'Header.php'; ?>
+
+<div class="card text-center">
+  <div class="card-header font-weight-bold">
+    Creator/About
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">About Nolan Trapp</h5>
+    <p class="card-text">Nolan Trapp is the lead content and creative design director of this and many websites</p><br />
+    <p class="card-text">He has training and fluence in about 17 different programming languages | 10 short of the world record</p><br />
+    <p class="card-text">He belives that everything that people create and make should be in the public and fair use licence</p><br />
+    <p class="card-text">If you have any questions, please contact him at: nolan@nolant108.tech</p><br />
+    <a href="https://wuffs.net" class="btn btn-primary">His Website</a>
+  </div>
+</div>
+
+<div class="card text-center">
+  <div class="card-header font-weight-bold">
+    Current Licence
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Copyright [2021] [https://wuffs.net] "Apache Licence-2.0" ©</h5>
+    <p class="card-text">Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at</p>
+<br />
+<p>    http://www.apache.org/licenses/LICENSE-2.0</p>
+<br />
+<p>    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.</p>
+<br />
+    <a href="https://www.apache.org/licenses/LICENSE-2.0" class="btn btn-primary">Licence Discription</a>
+  </div>
+  <div class="card-footer text-muted">
+    Updated on: 11/09/2021
+  </div>
+</div>
+
+<div class="alert alert-success" role="alert">
+<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAl9JREFUSEvtVj1z00AQfasZgzuSgprwC2IX1JAKmQZTWElFkl8AGSjA8gxixmcoYPA/wFAlchHT4HOFewr8E5KagtCFZKJlTtY5J9mObGOSFFF1cx/79r29fSfCBX10Qbi4Aj435S+X1KXygxzRybovOlv/IsFquVAPQI1m7WsvGWeIcdErLlw7PvwBYImAxo6Qm7OAO67dALAOYO8ok823vNaBGWcI2DgAAm3tiHZ9FmDFlomfRGc/+UJujAXuSxwotmDgS1PIohqvvbCXtt/KvUkSMPc6rq0kXlbnLMLKdlV2dYwYY5OtdYLbGkwFYEa9WZNKvrFf/zwv+6KTV5vGEVFrSWBVhxsABtKYkpnJJNGdSqEI5t1wnvDar0ovBHftFgEPAT7wRWdxiPFaxb4XML6FMjM2NTvHtTkpfell4SZR8Li/1/rcfNP+qcZOxe6CcVeNfSFDUk7F9sB4lZR7wNjMmNnKqxYwk9E1UqCWxd8ZuBVJth8EdEeBj9pvzoHokV9tt2JSm5npbFfdwlMGf4gxKN9/BqJ3MamZn/u1zvuQYaSQ7oioPX8lS2AwPpXkKJNdVH1nJjOYmz/w6eXQspry/zep47Xo30pTJga6TSFXwps6z8sV1SdsJ9M8zN7Wl25UI6e3E377Qi6kGogGUayvHx/2AoY3mYEg5wuZm8pATKcBuKcdSIEnTX6cfc1kmZHc+lWJOdBZVjlqbapHQgXQ0iqDMB1sWuBS2d4gwkcC9v9ksrnUZ1HXBghyaTVNS0aBA1Zvoh+BtGDzWr9c/1zzYnVWnCvG56FyiPEX8tOsLiiSjTsAAAAASUVORK5CYII="/> This version of the scouting software was pushed to live on 11/16/2021! <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAl9JREFUSEvtVj1z00AQfasZgzuSgprwC2IX1JAKmQZTWElFkl8AGSjA8gxixmcoYPA/wFAlchHT4HOFewr8E5KagtCFZKJlTtY5J9mObGOSFFF1cx/79r29fSfCBX10Qbi4Aj435S+X1KXygxzRybovOlv/IsFquVAPQI1m7WsvGWeIcdErLlw7PvwBYImAxo6Qm7OAO67dALAOYO8ok823vNaBGWcI2DgAAm3tiHZ9FmDFlomfRGc/+UJujAXuSxwotmDgS1PIohqvvbCXtt/KvUkSMPc6rq0kXlbnLMLKdlV2dYwYY5OtdYLbGkwFYEa9WZNKvrFf/zwv+6KTV5vGEVFrSWBVhxsABtKYkpnJJNGdSqEI5t1wnvDar0ovBHftFgEPAT7wRWdxiPFaxb4XML6FMjM2NTvHtTkpfell4SZR8Li/1/rcfNP+qcZOxe6CcVeNfSFDUk7F9sB4lZR7wNjMmNnKqxYwk9E1UqCWxd8ZuBVJth8EdEeBj9pvzoHokV9tt2JSm5npbFfdwlMGf4gxKN9/BqJ3MamZn/u1zvuQYaSQ7oioPX8lS2AwPpXkKJNdVH1nJjOYmz/w6eXQspry/zep47Xo30pTJga6TSFXwps6z8sV1SdsJ9M8zN7Wl25UI6e3E377Qi6kGogGUayvHx/2AoY3mYEg5wuZm8pATKcBuKcdSIEnTX6cfc1kmZHc+lWJOdBZVjlqbapHQgXQ0iqDMB1sWuBS2d4gwkcC9v9ksrnUZ1HXBghyaTVNS0aBA1Zvoh+BtGDzWr9c/1zzYnVWnCvG56FyiPEX8tOsLiiSjTsAAAAASUVORK5CYII="/>
+</div>
+
+
+
+<?php include 'Footer.php'; ?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
