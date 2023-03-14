585f3163f770c0a412c11ab5ee6126978c25a8d2
Nolan <officialstackemup@gmail.com>
2023-03-13 17:54:14 -0700

    Update index.php

--- schedule/index.php
+++ schedule/index.php
@@ -15,28 +15,13 @@
 
 <?php include '../Header.php'; ?>
 
-
-    <h1>HTML Form</h1>
-    <form method="" action="action.php">
-        Code: <input type="text" name="code"><br>
-	<p>Please select your type:</p>
-  <input type="radio" id="level" name="level" value="qual">
-  <label for="qual">Qualifications</label><br>
-  <input type="radio" id="level" name="level" value="playoff">
-  <label for="playoff">Playoffs</label><br></br>  
-	number: <input type="text" name="number"><br/>      
-	<br/>
-        <input type="submit" value="submit" >
-    </form>
-
-
     <form method="" action="action.php">
   <div class="form-group">
     <label for="exampleInputEmail1">Event Code</label>
-    <input name="code" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Event Code">
+    <input name="code" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter 4 or 5 char code.">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <label for="exampleFormControlSelect2">Choose Level</label>
     <select name="level" multiple class="form-control" id="exampleFormControlSelect2">
       <option>qual</option>
       <option>playoff</option>
@@ -44,7 +29,7 @@
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Team Number</label>
-    <input name="number" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Team Number">
+    <input name="number" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Team Number.">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
