b7b3460baac374ccbb42f1a58e1e80efb08f7881
Nolan <officialstackemup@gmail.com>
2023-03-13 19:32:08 -0700

    Update action.php

--- schedule/action.php
+++ schedule/action.php
@@ -21,30 +21,6 @@ $level= $_GET['level'];
 $frcCode = $_ENV('frcBASIC');
 
 
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://frc-api.firstinspires.org/v3.0/2023/schedule/caoc?tournamentLevel=qual&teamNumber=4984',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'If-Modified-Since: ',
-    'Authorization: Basic '.$frcCode
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-
-echo $response;
-
-echo $frcCode;
 
 ?>
 
