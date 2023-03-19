9610e136b84a04391e638e8caee43107aacb530c
Sparks <sparks@wuffs.net>
2023-03-19 12:53:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -164,14 +164,11 @@ $(document).ready(function(){
   $(".dropdown-toggle").dropdown("toggle");
 });
 
-
-const toggleTo3 = document.getElementById("toggle-to-3");
 const toggleTo2 = document.getElementById("toggle-to-2");
 const toggleTo1 = document.getElementById("toggle-to-1");
 
-const div1 = document.getElementById("div1");
+const div1 = document.getElementById("div-1");
 const div2 = document.getElementById("div2");
-const div3 = document.getElementById("div3");
 
 const hide = el => el.style.setProperty("display", "none");
 const show = el => el.style.setProperty("display", "block");
