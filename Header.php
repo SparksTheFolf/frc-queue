5d7e4ab0deb04b8f9fd140cf3e640bac8d94a416
Sparks <sparks@wuffs.net>
2023-03-19 15:07:09 -0700

    Update Header.php

--- Header.php
+++ Header.php
@@ -5,7 +5,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.js" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
@@ -47,9 +47,8 @@
       </li>
         </ul>
     </div>
-
     <li class="nav-item dropdown mr-auto">
-        <a class="mr-auto nav-link dropdown-toggle myQueue-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
+        <a class="mr-auto nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
           myQueue
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -69,10 +68,6 @@
 
 $(document).ready(function(){
   $(".dropdown-toggle").dropdown("toggle");
-});
-
-$(document).ready(function(){
-  $(".myQueue-toggle").dropdown("toggle");
 });
 
 </script>
