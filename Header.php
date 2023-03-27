9b83b9338f3986d77953eed88b81b5adce703e70
Sparks <sparks@wuffs.net>
2023-03-26 21:13:23 -0700

    Update Header.php

--- Header.php
+++ Header.php
@@ -22,7 +22,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light justify-content-center mx-auto justify-content-center">
   <div class="container-fluid mx-auto">
-    <a class="navbar-brand" href="/">FRC Queue</a>
+    <a class="navbar-brand mx-auto" href="/">FRC Queue</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
