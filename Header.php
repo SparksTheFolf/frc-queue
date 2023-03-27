a2d7f74314e05ce90b03a53adf72cd3c4ff8e84c
Sparks <sparks@wuffs.net>
2023-03-26 21:12:31 -0700

    wef

--- Header.php
+++ Header.php
@@ -8,8 +8,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.js" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+<style>
+.mainHeader {
+  background-color: white;
+}
+
+</style>
+
 </head>
 <body>
+
+<div class="mainHeader">
 
 <nav class="navbar navbar-expand-lg navbar-light justify-content-center mx-auto justify-content-center">
   <div class="container-fluid mx-auto">
@@ -63,7 +72,7 @@ $(document).ready(function(){
 });
 
 </script>
-
+</div>
 
 </body>
 </html>
