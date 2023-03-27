3f8cb493e40a5b5941fcf1a559d0683da6c47fb5
Sparks <sparks@wuffs.net>
2023-03-26 18:20:44 -0700

    wef

--- schedule/action.php
+++ schedule/action.php
@@ -1,9 +1,29 @@
 <html>
 <head>
-
-<link rel="stylesheet" href="styles.css">
-
-
+<style>
+.tg  {
+    border-collapse:collapse;
+    border-spacing:0; 
+    margin-left: auto;
+    margin-right: auto;
+    border: 14px outset #ff6550;
+}
+.tg td{
+    border-color:black;
+    border-style:solid;
+    border-width:1px;
+    font-family:Arial, sans-serif;
+    font-size:14px;
+  overflow:hidden;
+  padding:10px 5px;
+  word-break:normal;
+}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top
+}
+</style>
 <title>Match Schedule</title>
 
 </head>
