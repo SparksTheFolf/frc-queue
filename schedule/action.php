34d65c3f1579529a8791d0511bbe6e061fbb56c0
Sparks <sparks@wuffs.net>
2023-03-26 18:13:30 -0700

    Update action.php

--- schedule/action.php
+++ schedule/action.php
@@ -1,9 +1,32 @@
 <html>
 <head>
+<style>
+body {
+    background-color: lightblue;
+  }
 
-<link rel="stylesheet" href="/css/Generate-Action.css">
+.tg  {
+    border-collapse:collapse;
+    border-spacing:0; 
+    margin-left: auto;
+    margin-right: auto;
+}
+.tg td{
+    border-color:black;
+    border-style:solid;
+    border-width:1px;
+    font-family:Arial, sans-serif;
+    font-size:14px;
+  overflow:hidden;
+  padding:10px 5px;
+  word-break:normal;
+}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
 
-
+</style>
 <title>Match Schedule</title>
 
 </head>
